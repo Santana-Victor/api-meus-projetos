85117694646b6bcdc87f0c467c23046d0741c6df
Victor Santana <santanavictor592@gmail.com>
2024-10-02 14:39:44 -0300

    feat: configurar deploy no Render

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@ import 'dotenv/config';
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import routes from './routes/routes';
+const host = '0.0.0.0';
+const port = process.env.PORT ? Number(process.env.PORT) : 8080;
 
-const app = fastify({ logger: true });
+const app = fastify();
 
 async function startServer() {
   await app.register(cors);
   await app.register(routes);
 
   try {
-    await app.listen({ port: 8080 });
+    await app.listen({ host: host, port: port });
   } catch (error) {
     process.exit(1);
   }
