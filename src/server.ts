1d50c3eb6a3fbb06c8cfd1c5bd8a451d1eb500e2
Victor Santana <santanavictor592@gmail.com>
2024-10-03 11:15:12 -0300

    feat: add dominío de origem do Front-end da aplicação

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,12 @@ import cors from '@fastify/cors';
 import routes from './routes/routes';
 const host = '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 8080;
+const origin = process.env.DOMAIN_ACCESS_ALLOWED as string;
 
 const app = fastify();
 
 async function startServer() {
-  await app.register(cors);
+  await app.register(cors, { origin: origin });
   await app.register(routes);
 
   try {
